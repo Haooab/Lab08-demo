eee67b6061d37f416230ead86638e119638362e1
Huihao Peng <huihao@ualberta.ca>
2026-03-05 18:33:34 -0700

    failed test for deletecity and countcity

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,4 +1,5 @@
 package com.example.lab08;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -12,5 +13,24 @@ public class CustomListTest {
 
         // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
+    }
+    @Test
+    public void testdeleteCity(){
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        list.deleteCity(calgary);
+
+        // This will fail initially because hasCity() doesn't exist
+        assertFalse(list.hasCity(calgary));
+    }
+    @Test
+    public void testcountCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+
+        // This will fail initially because hasCity() doesn't exist
+        assertTrue(list.countCity()==1);
     }
 }
